808dda5b20e4af06f33789b4c93edf32bc27a61d
MeenalKaurLNG <meenalpreet.kaur@lngconsultancy.co.in>
2020-07-24 23:15:02 +0530

    IsPrimeOrNot

---
+++ PrimeOrNot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpProgramms
+{
+    class PrimeOrNot
+    {
+        private int number;
+        bool prime;
+
+        public int Number
+        {
+            set
+            {
+                this.number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+
+        public string Prime(int number)
+        {
+            for(int i=2;i<number/2;i++)
+            {
+                if(number % i == 0)
+                {
+                    prime = true;
+                    break;
+                }
+            }
+            if(prime == true)
+            {
+                return " is not a prime number";
+            }
+            else
+            {
+                return " is a prime number";
+            }
+
+        }
+        
+    }
+}
